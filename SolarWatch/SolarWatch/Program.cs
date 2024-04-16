99e58bef657eb3847908646d66ff96ee061e58e8
Laszlo Szijarto <szillas92@gmail.com>
2024-04-16 15:36:37 +0200

    Untracked appsettings.json file and added it to the gitignore file.

--- SolarWatch/SolarWatch/Program.cs
+++ SolarWatch/SolarWatch/Program.cs
@@ -1,3 +1,6 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using SolarWatch.CoordinateProvider;
 using SolarWatch.Data;
 using SolarWatch.JsonProcessor;
@@ -6,19 +9,14 @@ using SolarWatch.SunriseSunsetProvider;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var issuerKey = builder.Configuration["SolarWatch:SecretKey"];
+
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<ICoordinateDataProvider, OpenWeatherCoordDataProviderApi>();
-builder.Services.AddSingleton<ISunriseSunsetProvider, SunriseSunsetApi>();
-builder.Services.AddSingleton<IJsonProcessor, JsonProcessor>();
-builder.Services.AddDbContext<SolarWatchApiContext>();
-builder.Services.AddScoped<ICityRepository, CityRepository>();
-builder.Services.AddScoped<ISunriseSunsetRepository, SunriseSunsetRepository>();
-
+AddServices();
+ConfigureSwagger();
+AddDbContext();
+AddAuthentication();
+//AddIdentity();
 
 var app = builder.Build();
 
@@ -27,6 +25,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
@@ -36,3 +35,45 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+void AddServices()
+{
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSingleton<ICoordinateDataProvider, OpenWeatherCoordDataProviderApi>();
+    builder.Services.AddSingleton<ISunriseSunsetProvider, SunriseSunsetApi>();
+    builder.Services.AddSingleton<IJsonProcessor, JsonProcessor>();
+    builder.Services.AddScoped<ICityRepository, CityRepository>();
+    builder.Services.AddScoped<ISunriseSunsetRepository, SunriseSunsetRepository>();
+}
+
+void ConfigureSwagger()
+{
+    builder.Services.AddSwaggerGen();
+}
+
+void AddDbContext()
+{
+    builder.Services.AddDbContext<SolarWatchApiContext>();
+}
+
+void AddAuthentication()
+{
+    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ClockSkew = TimeSpan.Zero,
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = "",
+                ValidAudience = "",
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(issuerKey)
+                ),
+            };
+        });
+}
