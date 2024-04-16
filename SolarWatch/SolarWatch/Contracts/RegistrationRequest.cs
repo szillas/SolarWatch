936efb22c60918e3bfe345e7ee78ec93ca49a2f5
Laszlo Szijarto <szillas92@gmail.com>
2024-04-16 16:47:48 +0200

    Added RegistrationRequest and RegistrationResponse records.

---
+++ SolarWatch/SolarWatch/Contracts/RegistrationRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SolarWatch.Contracts;
+
+public record RegistrationRequest(
+    [Required]string Email,
+    [Required]string UserName,
+    [Required]string Password
+    );
---
+++ SolarWatch/SolarWatch/Contracts/RegistrationResponse.cs
@@ -0,0 +1,5 @@
+﻿namespace SolarWatch.Contracts;
+
+public record RegistrationResponse(
+    string Email,
+    string UserName);
