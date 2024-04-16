e1efc929cf68294dbb771064c7c083a2ab43c506
Laszlo Szijarto <szillas92@gmail.com>
2024-04-16 16:57:34 +0200

    Added AthResult record, IAuthService interface and its implementation, AuthService.

---
+++ SolarWatch/SolarWatch/Services/Authentication/AuthResult.cs
@@ -0,0 +1,11 @@
+﻿namespace SolarWatch.Services.Authentication;
+
+public record AuthResult(
+    bool Success,
+    string Email,
+    string UserName,
+    string Token)
+{
+    //Error code - error message
+    public readonly Dictionary<string, string> ErrorMessages = new Dictionary<string, string>();
+}
---
+++ SolarWatch/SolarWatch/Services/Authentication/AuthService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SolarWatch.Services.Authentication;
+
+public class AuthService : IAuthService
+{
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public AuthService(UserManager<IdentityUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task<AuthResult> RegisterAsync(string email, string userName, string password)
+        {
+            var result = await _userManager.CreateAsync(
+                new IdentityUser { UserName = userName, Email = email }, password);
+
+            if (!result.Succeeded)
+            {
+                return FailedRegistration(result, email, userName);
+            }
+
+            return new AuthResult(true, email, userName, "");
+        }
+    
+    private static AuthResult FailedRegistration(IdentityResult result, string email, string username)
+    {
+        var authResult = new AuthResult(false, email, username, "");
+
+        foreach (var error in result.Errors)
+        {
+            authResult.ErrorMessages.Add(error.Code, error.Description);
+        }
+
+        return authResult;
+    }
+}
