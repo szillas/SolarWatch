3d04e6d8fbd61a70141f074725861bd883179eeb
Laszlo Szijarto <szillas92@gmail.com>
2024-04-17 12:34:31 +0200

    Started working on getting 1 city in the citycontroller.

--- SolarWatch/SolarWatch/Controllers/CityController.cs
+++ SolarWatch/SolarWatch/Controllers/CityController.cs
@@ -17,8 +17,24 @@ public class CityController : ControllerBase
     }
     
     //Does it need to be asynchronous?
-    [HttpGet]
+    [HttpGet("Cities")]
     public async Task<IActionResult> GetAll()
+    {
+        try
+        {
+            var cities = await Task.Run(() => _cityRepository.GetAll());
+            return Ok(cities);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "An error occured while trying to list all cities.");
+            return StatusCode(500, "An error occured while trying to list all cities.");
+        }
+    }
+    
+    //Not yet finished
+    [HttpGet("{country}/{city}")]
+    public async Task<IActionResult> GetCity()
     {
         try
         {
